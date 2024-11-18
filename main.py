22baac17e23f65b8db7d83e2b1386bd7a3ac9111
BarMy49 <57178122+BarMy49@users.noreply.github.com>
2024-11-18 20:41:23 +0100

    First code snippet

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
 import sys
 import os
 picdir = "./pic"
 libdir = "./lib"
+fontdir = "./fonts"
+
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
@@ -23,81 +23,22 @@ try:
     epd.init()
     epd.Clear()
 
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
+    font60 = ImageFont.truetype(os.path.join(fontdir, 'JetBrainsMono/JetBrainsMono-BoldItalic.ttf'), 60)
 
-    # # Drawing on the Horizontal image
-#    logging.info("1.Drawing on the Horizontal image...")
-#    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-#    Other = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-#    draw_Himage = ImageDraw.Draw(Himage)
-#    draw_other = ImageDraw.Draw(Other)
-#    draw_Himage.text((10, 0), 'hello world', font = font24, fill = 0)
-#    draw_Himage.text((10, 20), '7.5inch e-Paper B', font = font24, fill = 0)
-#    draw_Himage.text((150, 0), u'微雪电子', font = font24, fill = 0)    
-#    draw_other.line((20, 50, 70, 100), fill = 0)
-#    draw_other.line((70, 50, 20, 100), fill = 0)
-#    draw_other.rectangle((20, 50, 70, 100), outline = 0)
-#    draw_other.line((165, 50, 165, 100), fill = 0)
-#    draw_Himage.line((140, 75, 190, 75), fill = 0)
-#    draw_Himage.arc((140, 50, 190, 100), 0, 360, fill = 0)
-#    draw_Himage.rectangle((80, 50, 130, 100), fill = 0)
-#    draw_Himage.chord((200, 50, 250, 100), 0, 360, fill = 0)
-#    epd.display(epd.getbuffer(Himage),epd.getbuffer(Other))
-#    time.sleep(2)
-
-    
-    # Drawing on the Vertical image
-#    logging.info("2.Drawing on the Vertical image...")
-#    Limage = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-#    Limage_Other = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame
-#    draw_Himage = ImageDraw.Draw(Limage)
-#    draw_Himage_Other = ImageDraw.Draw(Limage_Other)
-#    draw_Himage.text((2, 0), 'hello world', font = font18, fill = 0)
-#    draw_Himage.text((2, 20), '7.5inch epd', font = font18, fill = 0)
-#    draw_Himage_Other.text((20, 50), u'微雪电子', font = font18, fill = 0)
-#    draw_Himage_Other.line((10, 90, 60, 140), fill = 0)
-#    draw_Himage_Other.line((60, 90, 10, 140), fill = 0)
-#    draw_Himage_Other.rectangle((10, 90, 60, 140), outline = 0)
-#    draw_Himage_Other.line((95, 90, 95, 140), fill = 0)
-#    draw_Himage.line((70, 115, 120, 115), fill = 0)
-#    draw_Himage.arc((70, 90, 120, 140), 0, 360, fill = 0)
-#    draw_Himage.rectangle((10, 150, 60, 200), fill = 0)
-#    draw_Himage.chord((70, 150, 120, 200), 0, 360, fill = 0)
-#    epd.display(epd.getbuffer(Limage), epd.getbuffer(Limage_Other))
-#    time.sleep(2)
-
-#    logging.info("3.read bmp file")
-#    epd.init_Fast()
-#    Himage = Image.open(os.path.join(picdir, '7in5_V2_b.bmp'))
-#    Himage_Other = Image.open(os.path.join(picdir, '7in5_V2_r.bmp'))
-#    epd.display(epd.getbuffer(Himage),epd.getbuffer(Himage_Other))
-#    time.sleep(2)
-
-    # partial update
-    logging.info("4.show time")
-    epd.init()
-    epd.display_Base_color(0xFF)
-    epd.init_part()
-    Himage = Image.new('1', (epd.width, epd.height), 0)
+    logging.info("1.Drawing on the Horizontal image...")
+    Himage = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
     draw_Himage = ImageDraw.Draw(Himage)
-    while (True):
-        draw_Himage.rectangle((10, 120, 130, 170), fill = 0)
-        draw_Himage.text((10, 120), time.strftime('%H:%M:%S'), font = font24, fill = 255)
-        epd.display_Partial(epd.getbuffer(Himage),0, 0, epd.width, epd.height)
-
-
-    logging.info("Clear...")
-    epd.init()
-    epd.Clear()
+    draw_Himage.text((10, 10), '@Pabilo', font = font60, fill = 0)
+    epd.display(epd.getbuffer(Himage))
 
     logging.info("Goto Sleep...")
     epd.sleep()
-    
+
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
+
+except KeyboardInterrupt:
     logging.info("ctrl + c:")
     epd7in5b_V2.epdconfig.module_exit(cleanup=True)
     exit()
